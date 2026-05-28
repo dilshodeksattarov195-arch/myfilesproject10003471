b75eb1b46f628060277924320089d4cfca1e2f7c
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-28 06:33:34 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class clusterDetchWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = clusterDetchWorker()
+    print("Worker engine initialized.")
